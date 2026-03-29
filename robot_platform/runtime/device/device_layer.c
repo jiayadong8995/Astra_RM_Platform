e3e0ad4de48df751db987e2838d2653b4b01d722
jiayadong1 <jiayadong1@xiaomi.com>
2026-03-29 19:32:20 +0800

    refactor(runtime): add device layer entrypoint

---
+++ robot_platform/runtime/device/device_layer.c
@@ -0,0 +1,184 @@
+#include "device_layer.h"
+
+#include "actuator/motor/motor_actuator_device.h"
+#include "imu/bmi088_device.h"
+#include "remote/dbus_remote_device.h"
+
+static void platform_map_device_command_to_motor_set(const platform_device_command_t *command,
+                                                     platform_motor_command_set_t *motor_set);
+static void platform_copy_feedback_to_input(const platform_device_feedback_t *feedback,
+                                            platform_device_input_t *input);
+
+void platform_device_layer_bind_default(platform_device_layer_t *layer)
+{
+  platform_bmi088_device_bind_default(&layer->imu);
+  platform_dbus_remote_device_bind_default(&layer->remote);
+  platform_motor_actuator_device_bind_default(&layer->motor);
+  layer->input_sequence = 0U;
+  layer->feedback_sequence = 0U;
+  layer->command_sequence = 0U;
+}
+
+platform_device_result_t platform_device_layer_init(platform_device_layer_t *layer)
+{
+  platform_device_result_t result = PLATFORM_DEVICE_RESULT_OK;
+
+  if (layer->imu.ops.init != 0)
+  {
+    result = layer->imu.ops.init(&layer->imu);
+    if (result != PLATFORM_DEVICE_RESULT_OK)
+    {
+      return result;
+    }
+  }
+
+  if (layer->remote.ops.init != 0)
+  {
+    result = layer->remote.ops.init(&layer->remote);
+    if (result != PLATFORM_DEVICE_RESULT_OK)
+    {
+      return result;
+    }
+  }
+
+  if (layer->motor.ops.init != 0)
+  {
+    result = layer->motor.ops.init(&layer->motor);
+    if (result != PLATFORM_DEVICE_RESULT_OK)
+    {
+      return result;
+    }
+  }
+
+  return PLATFORM_DEVICE_RESULT_OK;
+}
+
+platform_device_result_t platform_device_layer_read_input(platform_device_layer_t *layer,
+                                                          platform_device_input_t *input)
+{
+  platform_device_result_t result = PLATFORM_DEVICE_RESULT_OK;
+  platform_device_feedback_t feedback = {0};
+
+  input->sequence = ++layer->input_sequence;
+
+  if (layer->imu.ops.read_sample != 0)
+  {
+    result = layer->imu.ops.read_sample(&layer->imu, &input->imu_sample);
+    if (result != PLATFORM_DEVICE_RESULT_OK && result != PLATFORM_DEVICE_RESULT_UNAVAILABLE)
+    {
+      return result;
+    }
+  }
+
+  if (layer->remote.ops.read_input != 0)
+  {
+    result = layer->remote.ops.read_input(&layer->remote, &input->rc_input);
+    if (result != PLATFORM_DEVICE_RESULT_OK && result != PLATFORM_DEVICE_RESULT_UNAVAILABLE)
+    {
+      return result;
+    }
+  }
+
+  if (layer->motor.ops.read_motor_feedback != 0)
+  {
+    result = layer->motor.ops.read_motor_feedback(&layer->motor, &feedback);
+    if (result != PLATFORM_DEVICE_RESULT_OK && result != PLATFORM_DEVICE_RESULT_UNAVAILABLE)
+    {
+      return result;
+    }
+    platform_copy_feedback_to_input(&feedback, input);
+  }
+
+  return PLATFORM_DEVICE_RESULT_OK;
+}
+
+platform_device_result_t platform_device_layer_write_command(platform_device_layer_t *layer,
+                                                             const platform_device_command_t *command)
+{
+  platform_motor_command_set_t motor_set = {0};
+
+  if (layer->motor.ops.write_motor_command == 0)
+  {
+    return PLATFORM_DEVICE_RESULT_UNSUPPORTED;
+  }
+
+  layer->command_sequence++;
+  platform_map_device_command_to_motor_set(command, &motor_set);
+  return layer->motor.ops.write_motor_command(&layer->motor, &motor_set);
+}
+
+platform_device_result_t platform_device_layer_read_feedback(platform_device_layer_t *layer,
+                                                             platform_device_feedback_t *feedback)
+{
+  if (layer->motor.ops.read_motor_feedback == 0)
+  {
+    return PLATFORM_DEVICE_RESULT_UNSUPPORTED;
+  }
+
+  feedback->sequence = ++layer->feedback_sequence;
+  return layer->motor.ops.read_motor_feedback(&layer->motor, feedback);
+}
+
+static void platform_map_device_command_to_motor_set(const platform_device_command_t *command,
+                                                     platform_motor_command_set_t *motor_set)
+{
+  motor_set->left_leg_joint[0].control_mode = command->joints[0].control_mode;
+  motor_set->left_leg_joint[0].torque_target = command->joints[0].torque_target;
+  motor_set->left_leg_joint[0].velocity_target = command->joints[0].velocity_target;
+  motor_set->left_leg_joint[0].position_target = command->joints[0].position_target;
+  motor_set->left_leg_joint[0].current_target = command->joints[0].current_target;
+  motor_set->left_leg_joint[0].kp = command->joints[0].kp;
+  motor_set->left_leg_joint[0].kd = command->joints[0].kd;
+  motor_set->left_leg_joint[0].valid = command->joints[0].valid;
+
+  motor_set->left_leg_joint[1].control_mode = command->joints[1].control_mode;
+  motor_set->left_leg_joint[1].torque_target = command->joints[1].torque_target;
+  motor_set->left_leg_joint[1].velocity_target = command->joints[1].velocity_target;
+  motor_set->left_leg_joint[1].position_target = command->joints[1].position_target;
+  motor_set->left_leg_joint[1].current_target = command->joints[1].current_target;
+  motor_set->left_leg_joint[1].kp = command->joints[1].kp;
+  motor_set->left_leg_joint[1].kd = command->joints[1].kd;
+  motor_set->left_leg_joint[1].valid = command->joints[1].valid;
+
+  motor_set->right_leg_joint[0].control_mode = command->joints[2].control_mode;
+  motor_set->right_leg_joint[0].torque_target = command->joints[2].torque_target;
+  motor_set->right_leg_joint[0].velocity_target = command->joints[2].velocity_target;
+  motor_set->right_leg_joint[0].position_target = command->joints[2].position_target;
+  motor_set->right_leg_joint[0].current_target = command->joints[2].current_target;
+  motor_set->right_leg_joint[0].kp = command->joints[2].kp;
+  motor_set->right_leg_joint[0].kd = command->joints[2].kd;
+  motor_set->right_leg_joint[0].valid = command->joints[2].valid;
+
+  motor_set->right_leg_joint[1].control_mode = command->joints[3].control_mode;
+  motor_set->right_leg_joint[1].torque_target = command->joints[3].torque_target;
+  motor_set->right_leg_joint[1].velocity_target = command->joints[3].velocity_target;
+  motor_set->right_leg_joint[1].position_target = command->joints[3].position_target;
+  motor_set->right_leg_joint[1].current_target = command->joints[3].current_target;
+  motor_set->right_leg_joint[1].kp = command->joints[3].kp;
+  motor_set->right_leg_joint[1].kd = command->joints[3].kd;
+  motor_set->right_leg_joint[1].valid = command->joints[3].valid;
+
+  motor_set->left_wheel.control_mode = command->wheels[0].control_mode;
+  motor_set->left_wheel.torque_target = command->wheels[0].torque_target;
+  motor_set->left_wheel.velocity_target = command->wheels[0].velocity_target;
+  motor_set->left_wheel.position_target = command->wheels[0].position_target;
+  motor_set->left_wheel.current_target = command->wheels[0].current_target;
+  motor_set->left_wheel.kp = command->wheels[0].kp;
+  motor_set->left_wheel.kd = command->wheels[0].kd;
+  motor_set->left_wheel.valid = command->wheels[0].valid;
+
+  motor_set->right_wheel.control_mode = command->wheels[1].control_mode;
+  motor_set->right_wheel.torque_target = command->wheels[1].torque_target;
+  motor_set->right_wheel.velocity_target = command->wheels[1].velocity_target;
+  motor_set->right_wheel.position_target = command->wheels[1].position_target;
+  motor_set->right_wheel.current_target = command->wheels[1].current_target;
+  motor_set->right_wheel.kp = command->wheels[1].kp;
+  motor_set->right_wheel.kd = command->wheels[1].kd;
+  motor_set->right_wheel.valid = command->wheels[1].valid;
+}
+
+static void platform_copy_feedback_to_input(const platform_device_feedback_t *feedback,
+                                            platform_device_input_t *input)
+{
+  input->actuator_feedback = feedback->actuator_feedback;
+}
--- robot_platform/runtime/device/device_layer.h
+++ robot_platform/runtime/device/device_layer.h
@@ -7,4 +7,26 @@
 #include "imu/imu_device.h"
 #include "remote/remote_device.h"
 
+typedef struct {
+  platform_imu_device_t imu;
+  platform_remote_device_t remote;
+  platform_motor_device_t motor;
+  uint32_t input_sequence;
+  uint32_t feedback_sequence;
+  uint32_t command_sequence;
+} platform_device_layer_t;
+
+void platform_device_layer_bind_default(platform_device_layer_t *layer);
+
+platform_device_result_t platform_device_layer_init(platform_device_layer_t *layer);
+
+platform_device_result_t platform_device_layer_read_input(platform_device_layer_t *layer,
+                                                          platform_device_input_t *input);
+
+platform_device_result_t platform_device_layer_write_command(platform_device_layer_t *layer,
+                                                             const platform_device_command_t *command);
+
+platform_device_result_t platform_device_layer_read_feedback(platform_device_layer_t *layer,
+                                                             platform_device_feedback_t *feedback);
+
 #endif
